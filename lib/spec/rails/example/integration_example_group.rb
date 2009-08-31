319933e41330882d1c6c8d3a7593f7831f7aeda9
George Ogata <george.ogata@gmail.com>
2009-08-31 01:22:05 -0400

    Ensure integration specs have access to matchers implemented via method_missing.

--- lib/spec/rails/example/integration_example_group.rb
+++ lib/spec/rails/example/integration_example_group.rb
@@ -10,6 +10,20 @@ module Spec
     module Example
       class IntegrationExampleGroup < ActionController::IntegrationTest
         Spec::Example::ExampleGroupFactory.register(:integration, self)
+
+        # Although the modules below are included in AS::TestCase, we
+        # need to include them again here since in rails (<= 2.3.3),
+        # AC::IntegrationTest fails to propagate unhandled
+        # method_missings up the ancestor chain, and some of these
+        # modules rely on method_missing.  Rails bug filed at:
+        # http://rails.lighthouseapp.com/projects/8994/tickets/3119
+        #
+        # We dup the modules to force ruby to include them.  Otherwise
+        # ruby detects that the modules are included further up the
+        # chain, and skips them.
+        include ::Spec::Matchers.dup
+        include ::Spec::Rails::Matchers.dup
+        include ::Spec::Rails::Mocks.dup
       end
     end
   end
---
+++ spec/spec/rails/example/integration_example_group_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "An integration example group", :type => :integration do
+  it "should provide access to RSpec matchers" do
+    true.should be_true
+  end
+end
